26c1ff6c1f152dcb8082c9c6067d1cebd3261b8a
Official-Vicks <chidieberevictory218@gmail.com>
2026-03-16 22:40:10 +0100

    add scroll animation

--- attendance_app/frontend/js/main.js
+++ attendance_app/frontend/js/main.js
@@ -297,3 +297,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+// Show button when user scrolls down
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+// Scroll to top when clicked
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
