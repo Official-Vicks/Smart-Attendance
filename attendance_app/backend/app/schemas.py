0c3c4a48c1650c094085f7118486cb8b9840d8de
Official-Vicks <chidieberevictory218@gmail.com>
2026-04-21 12:06:20 +0100

    Add files via upload

--- attendance_app/backend/app/schemas.py
+++ attendance_app/backend/app/schemas.py
@@ -78,6 +78,7 @@ class StudentOut(StudentBase):
     id: uuid.UUID
     profile_image: Optional[str] = None
     school_id: uuid.UUID
+    school_name: str
 
     model_config = {"from_attributes": True}
 
